8a065d60f07df57525de97f4ef9592bf62fcd891
JesusReignsX <samueljeffers21@hotmail.com>
2026-09-13 20:13:03 +0100

    Update gamble.js

--- src/commands/Economy/gamble.js
+++ src/commands/Economy/gamble.js
@@ -40,7 +40,8 @@ export default {
                 const remaining = lastGamble + GAMBLE_COOLDOWN - now;
                 const minutes = Math.floor(remaining / (1000 * 60));
                 const seconds = Math.floor((remaining % (1000 * 60)) / 1000);
-
+                );
+            }
 
                 throw createError(
                     "Gamble cooldown active",
